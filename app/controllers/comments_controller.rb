dad7e5d9c8ebb46a2d1be6fa0220f37a509be635
James Anderson <james.p.anderson16@gmail.com>
2017-02-11 20:57:27 -0700

    started comments but create/redirect is not functioning

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  def create
+    # @job = Job.find(params[:job_id]
+    @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
+
+    @comment.save
+        # flash[:success] = "You created a comment for #{@job.title}"
+    # redirect_to company_job_path(company, job)
+    redirect_to company_job_path(@comment.job)
+    end
+
+    # def show
+    #   @job = Job.find(params[:job_id]
+    #   @comment = Comment.find(params[:id])
+    # end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:author_name, :body)
+    end
+end
